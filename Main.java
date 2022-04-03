486ddd75d94bef784d8b832cf83853f797244029
Tyler Hawkins <thawkin3@byu.net>
2022-04-02 20:14:58 -0600

    feat: adds hello world example

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+  public static int add(int a, int b) {
+    return a + b;
+  }
+
+  public static int subtract(int a, int b) {
+    return a - b;
+  }
+
+  public static int multiply(int a, int b) {
+    return a * b;
+  }
+
+  public static int divide(int a, int b) {
+    return a / b;
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println(add(6, 2));
+    System.out.println(subtract(6, 2));
+    System.out.println(multiply(6, 2));
+    System.out.println(divide(6, 2));
+  }
+}
