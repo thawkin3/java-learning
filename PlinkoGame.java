99bff5d47adf2bac368419b474b6e8652d4defbe
Tyler Hawkins <thawkin3@byu.net>
2022-04-02 20:47:23 -0600

    refactor: simplifies dropPlinkoChip method

--- PlinkoGame.java
+++ PlinkoGame.java
@@ -1,52 +1,53 @@
 // https://pressbooks.howardcc.edu/jrip3/chapter/so-you-want-to-win-plinko/
 
 public class PlinkoGame {
-  private int boardColumns = 9;
-  private int boardRows = 13;
-  
-  public static double dropPlinkoChip() {
+  private static int numberOfBoardColumns = 9;
+  private static int numberOfBoardRows = 13;
+
+  public static void main(String[] args) {
+    System.out.println("Hello PlinkoGame");
+    dropPlinkoChips(10);
+  }
+
+  private static void dropPlinkoChips(int numberOfChips) {
+    if (numberOfChips < 1) {
+      dropPlinkoChip();
+    } else {
+      for (int i = 0; i < numberOfChips; i++) {
+        dropPlinkoChip();
+      }
+    }
+  }
+
+  private static double dropPlinkoChip() {
     System.out.println("Dropping a Plinko chip!");
     double chipX = 5.0;
+    final double moveLeftAmount = -0.5;
+    final double moveRightAmount = 0.5;
+    final double leftMostColumn = 1.0;
+    final double rightMostColumn = 9.0;
 
     // The chip is always in column 5 (chipX = 5) for row 1 (chipY = 1)
-    for (int chipY = 1; chipY < 13; chipY++) {
+    for (int chipY = 1; chipY < numberOfBoardRows; chipY++) {
       double previousChipX = chipX;
-      String movementDirection;
-      if (chipY % 2 == 0) {
-        // Even row
+
+      if (chipX == leftMostColumn) {
+        // Chip is already on the very left side of the board and can only move right
+        chipX += moveRightAmount;
+      } else if (chipX == rightMostColumn) {
+        // Chip is already on the very right side of the board and can only move left
+        chipX += moveLeftAmount;
+      } else {
         // Chip is able to move either left or right
         double randomNumber = Math.random();
-        double movementX = randomNumber > 0.5 ? 0.5 : -0.5;
-        movementDirection = randomNumber > 0.5 ? "right" : "left";
+        double movementX = randomNumber > 0.5 ? moveRightAmount : moveLeftAmount;
         chipX += movementX;
-      } else {
-        // Odd row
-        if (chipX == 1) {
-          // Chip is already on the very left side of the board and can only move right
-          chipX = 1.5;
-          movementDirection = "right";
-        } else if (chipX == 9) {
-          // Chip is already on the very right side of the board and can only move left
-          chipX = 8.5;
-          movementDirection = "left";
-        } else {
-          // Chip is able to move either left or right
-          double randomNumber = Math.random();
-          double movementX = randomNumber > 0.5 ? 0.5 : -0.5;
-          movementDirection = randomNumber > 0.5 ? "right" : "left";
-          chipX += movementX;
-        }
       }
 
-      System.out.println("Starting in " + chipY + ", " + previousChipX + "; moved " + movementDirection + " to " + (chipY + 1) + ", " + chipX);
+      System.out.println("Starting in " + chipY + ", " + previousChipX + "; moved to " + (chipY + 1) + ", " + chipX);
     }
 
     System.out.println("Final chipX: " + chipX);
     return chipX;
-  }
-
-  public static void main(String[] args) {
-    System.out.println("Hello PlinkoGame");
-    dropPlinkoChip();
   }
 }
